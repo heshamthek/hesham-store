8b5f23f564a0f10a312eb1ff81acc908af0164d1
heshamthek <hnajem133@gmail.com>
2024-08-29 10:37:12 +0300

    day 3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,35 @@
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="flex h-screen bg-gray-100">
-   
+<body   >
+<nav class="flex items-center justify-between flex-wrap bg-gray-500 p-1.5">
+  <div class="flex items-center flex-shrink-0 text-white mr-6">
+    <span class="font-semibold text-xl tracking-tight">Hesham store </span>
+  </div>
+  <div class="block lg:hidden">
+    <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+      <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+    </button>
+  </div>
+  <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div class="text-sm lg:flex-grow">
+      <a href="/landing" class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+      Landing
+      </a>
+    </div>
+    <div>
+    <form method="POST" action="{{ route('logout') }}" class="block py-2 px-4">
+    @csrf
+      <a href="{{ route('logout') }}" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">{{ __('Log Out') }}</a>
+      </form>
+    </div>
+  </div>
+</nav>
+
+<div class="flex h-screen bg-gray-100">
+
     <!-- Sidebar -->
-    <div id="sidebar" class="fixed inset-y-0 left-0 transform -translate-x-full md:translate-x-0 md:static md:w-64 bg-blue-900 text-white transition-transform duration-300 ease-in-out">
+    <div id="sidebar" class="fixed inset-y-0 left-0 transform -translate-x-full md:translate-x-0 md:static md:w-64 bg-gray-500 text-white transition-transform duration-300 ease-in-out">
         <div class="flex items-center justify-between p-4">
             <h1 class="text-lg font-bold">{{ Auth::user()->name }}</h1>
             <!-- Close Button for Mobile -->
@@ -21,17 +46,9 @@
             </button>
         </div>
         <nav class="p-4">
-
-           <a href="/profile/edit" class="block py-2 px-4 hover:bg-blue-800 rounded">{{ __('Profile') }}</a>
-
-            <a href="/landing" class="block py-2 px-4 hover:bg-blue-800 rounded">Landing</a>
             <a href="{{ route('categories.index') }}" class="block py-2 px-4 hover:bg-blue-800 rounded">Categories</a>
             <a href="{{ route('products.index') }}" class="block py-2 px-4 hover:bg-blue-800 rounded">Products</a>
-            <form method="POST" action="{{ route('logout') }}" class="block py-2 px-4">
-                @csrf
-                <a href="{{ route('logout') }}" onclick="event.preventDefault(); this.closest('form').submit();" class="block hover:bg-blue-800 rounded">
-                    {{ __('Log Out') }}
-                </a>
+                  </a>
             </form>
         </nav>
     </div>
@@ -47,7 +64,7 @@
         
         @yield('content')
     </div>
-
+    </div>
     <script>
         const sidebar = document.getElementById('sidebar');
         const openSidebarBtn = document.getElementById('open-sidebar');
